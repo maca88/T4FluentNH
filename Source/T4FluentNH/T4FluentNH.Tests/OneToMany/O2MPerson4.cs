4104f11693c6f5ce618c3c32b50befc2eeb919d4
bostjanm <bostjan.markezic@actual-it.si>
2015-11-13 15:34:25 +0100

    added support for custom column names

---
+++ Source/T4FluentNH/T4FluentNH.Tests/OneToMany/O2MPerson4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace T4FluentNH.Tests.OneToMany
+{
+    public partial class O2MPerson4 : Domain.Entity
+    {
+        public virtual string Name { get; set; }
+
+        //Bidirectional association with a non convention key
+        public virtual ISet<O2MCar4> Cars
+        {
+            get { return _cars ?? (_cars = new HashSet<O2MCar4>()); }
+            set { _cars = value; }
+        }
+
+    }
+
+    public partial class O2MCar4 : Domain.Entity
+    {
+        public virtual string Model { get; set; }
+
+        [NotNull]
+        public virtual O2MPerson4 Owner { get; set; }
+
+        public virtual int OwnerId { get; set; }
+    }
+
+    public class O2MCar4Mapping : IAutoMappingOverride<O2MCar4>
+    {
+        public void Override(AutoMapping<O2MCar4> mapping)
+        {
+            mapping.References(o => o.Owner).Column("Owner_CustomFK");
+        }
+    }
+
+    public class O2MPerson4Mapping : IAutoMappingOverride<O2MPerson4>
+    {
+        public void Override(AutoMapping<O2MPerson4> mapping)
+        {
+            mapping.HasMany(o => o.Cars).KeyColumn("Owner_CustomFK").Inverse();
+        }
+    }
+}
